6506b8fcd05c8380c8e77fc42ef547b2a7e2bb3a
Karthik Subramanian <karthik.s.sundaram@gmail.com>
2013-02-07 23:55:22 +0530

    better css on chat page

--- www/js/lib/chat.js
+++ www/js/lib/chat.js
@@ -107,8 +107,8 @@ $(document).ready(function() {
 
 
 	                                socket.on("groupChat", function(data) {
-                                        var tmpl_you = "<div class='arrow_box you'><p><b>{{sender}}</b> : {{message}}</p></div><br>";
-                                        var tmpl_me = "<div class='arrow_box me'><p><b>{{sender}}</b> : {{message}}</p></div><br>";
+                                        var tmpl_you = "<div class='arrow_box you'><strong>{{sender}}</strong> : {{message}}</div><br>";
+                                        var tmpl_me = "<div class='arrow_box me'><strong>{{sender}}</strong> : {{message}}</div><br>";
 		                                if(nick === data.sender){
                                             var html = Mustache.to_html(tmpl_me, data);
                                         }
